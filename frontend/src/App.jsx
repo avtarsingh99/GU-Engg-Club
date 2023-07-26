10fdba7d743dcda2f4d77d39546852564d3c1224
Rakesh Dhariwal <rakeshdhariwal61@gmail.com>
2023-07-26 15:11:20 +0530

    file structure added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,7 @@
-import { Button, Container, Text } from "@chakra-ui/react"
+import Home from "./pages/Home"
 function App() {
-  const handleChange = (e)=>{
-    console.log(e.target.value);
-  }
-  const debounceSearch = (func, timeout = 300)=>{
-    let timer;
-    return (...args)=>{
-      clearTimeout(timer);
-      timer = setTimeout(()=>{
-        func.apply(this, args);
-      }, timeout)
-    }
-  };
-  const betterSearch = debounceSearch((e)=>handleChange(e));
-  console.log(betterSearch);
   return (
-    <Container minWidth={'100dvw'} minH={'100dvh'} display={'flex'} alignItems={'center'} justifyContent={'center'}>
-      <input placeholder="search" onChange ={(e)=>betterSearch(e)}></input>
-    </Container>
+    <Home/>
   )
 }
 
